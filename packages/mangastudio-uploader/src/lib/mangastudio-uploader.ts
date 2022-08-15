e64f977666d5bdc394302f4ab1239af4c860965c
Satish Babariya <satish.babariya@gmail.com>
2022-08-15 16:11:28 +0530

    add reperscan scrapper

--- packages/mangastudio-uploader/src/lib/mangastudio-uploader.ts
+++ packages/mangastudio-uploader/src/lib/mangastudio-uploader.ts
@@ -1,3 +1,20 @@
-export function mangastudioUploader(): string {
-  return 'mangastudio-uploader';
+interface Manga {
+  id: number;
 }
+
+interface Chapter {
+  name: string;
+  date: string;
+  images: string[];
+}
+
+async function uploadChapter(manga: Manga, chapter: Chapter) {
+  // await axios.post('https://mangastudio.app/api/chapters', {
+  //   mangaId: manga.id,
+  //   name: chapter.name,
+  //   date: chapter.date,
+  //   images: chapter.images,
+  // });
+}
+
+export { uploadChapter, Manga, Chapter };
--- packages/reaperscans/src/main.ts
+++ packages/reaperscans/src/main.ts
@@ -1,3 +1,51 @@
-import {} from '@mangastudio-scrapper/mangastudio-uploader';
+import { uploadChapter } from '@mangastudio-scrapper/mangastudio-uploader';
+import axios from 'axios';
+import { URL } from 'node:url';
+import * as cheerio from 'cheerio';
+import * as list from './list.json';
 
-console.log('Hello World!');
+async function getManga(url: string) {
+  const html = await axios(url);
+  const $ = cheerio.load(html.data);
+  const chapters = $('.wp-manga-chapter a');
+  const chapterUrls = chapters.map((i, el) => $(el).attr('href')).get();
+  const chapterNames = chapters.map((i, el) => $(el).text()).get();
+  const chapterDates = $('.chapter-release-date')
+    .map((i, el) => $(el).text())
+    .get();
+
+  return chapterUrls.map((url, i) => ({
+    url: new URL(url).href,
+    name: chapterNames[i],
+    date: chapterDates[i],
+  }));
+}
+
+async function getChapter(url: string) {
+  const html = await axios(url);
+  const $ = cheerio.load(html.data);
+  const pages = $('.reading-content img')
+    .map((i, el) => new URL($(el).attr('data-src')).href)
+    .get();
+  return pages;
+}
+
+async function main() {
+  console.log('Starting...');
+  list.forEach(async (manga) => {
+    const chapters = await getManga(manga.url);
+    chapters.forEach(async (chapter) => {
+      const pages = await getChapter(chapter.url);
+      await uploadChapter(
+        { id: manga.id },
+        {
+          name: chapter.name,
+          date: chapter.date,
+          images: pages,
+        }
+      );
+    });
+  });
+}
+
+main();
